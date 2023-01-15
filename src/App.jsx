b11e8c3022997c4365ab60d6274fed9c623deb31
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-15 23:44:43 +0300

    refactor: updated to react router dom v6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {
-  NavLink,
-  Redirect,
   Route,
-  Switch,
+  Navigate,
+  NavLink,
+  Outlet,
+  Routes,
   useParams,
-  useRouteMatch
 } from 'react-router-dom';
 
 const users = [1, 2, 3, 4, 5];
@@ -13,12 +13,11 @@ const users = [1, 2, 3, 4, 5];
 const MainPage = () => <h1>Main Page</h1>;
 
 const UsersListPage = () => {
-  const { path } = useRouteMatch();
   return (
     <>
       <h1>Users List</h1>
       {users.map(user => (
-        <div key={user}><NavLink to={`${path}/${user}`}>User {user}</NavLink></div>
+        <div key={user}><NavLink to={user + '/profile'}>User {user}</NavLink></div>
       ))}
     </>
   );
@@ -31,15 +30,15 @@ const EditUserPage = () => {
             <h1>Edit User Page</h1>
             <ul>
                 <li>
-                    <NavLink to={"/users/" + userId}>User profile</NavLink>
+                    <NavLink to={'/users/' + userId}>User profile</NavLink>
                 </li>
                 <li>
-                    <NavLink to={"/users/" + (Number(userId) + 1)}>
+                    <NavLink to={'/users/' + (Number(userId) + 1)}>
                         Next User
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to={"/users"}> Users List page</NavLink>
+                    <NavLink to={'/users'}> Users List page</NavLink>
                 </li>
             </ul>
         </>
@@ -47,30 +46,18 @@ const EditUserPage = () => {
 };
 
 const UsersLayout = () => {
-  const { path } = useRouteMatch();
-    return (
-        <div>
-            <h1>Users Layout</h1>
-            <NavLink to='/'>Main Page</NavLink>
-            <Switch>
-                <Route
-                    path={path + "/:userId/profile"}
-                    component={UserProfilePage}
-                />
-                <Route path={path + "/:userId/edit"} component={EditUserPage} />
-                <Route path={path} exact component={UsersListPage} />
-                <Redirect
-                    from={path + "/:userId"}
-                    to={path + "/:userId/profile"}
-                />
-            </Switch>
-        </div>
+  return (
+    <>
+      <h1>Users Layout</h1>
+      <NavLink to='/'>Main Page</NavLink>
+      <Outlet/>
+    </>
     );
 };
  
 const UserProfilePage = () => {
   const { userId } = useParams();
-    return (
+  return (
         <div>
             <h1>UserPage</h1>
                 <div>
@@ -92,11 +79,19 @@ function App() {
     <>
       <h1>App</h1>
       <NavLink to='/users'>Users list Page</NavLink>
-      <Switch>
-          <Route path='/users' component={UsersLayout} />
-          <Route path='/' component={MainPage} />
-          <Redirect to='/' />
-      </Switch>
+      <Routes>
+        <Route path='/' element={<MainPage/>} />
+        <Route path='/users' element={<UsersLayout />}>
+          <Route index element={<UsersListPage />} />
+          <Route path=':userId' element={<Outlet />}>
+            <Route path='profile' element={<UserProfilePage />} />
+            <Route path='edit' element= {<EditUserPage />} />
+            <Route index element= {<Navigate to='./profile' />} />
+            <Route path='*' element={<Navigate to='../profile' />} />
+          </Route>
+        </Route>
+        <Route path='*' element={<Navigate to='/' />} />
+      </Routes>
     </>
   );
 };
